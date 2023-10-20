f8584897bd25b4e43067a734b6ebbadb98b692bf
u.ezinnaCOS101 <itzudochukwu@gmail.com>
2023-10-19 19:36:28 -0500

    Week-2

---
+++ week-2/practice-2.rs
@@ -0,0 +1,7 @@
+fn main() {
+	println!(); // Print just a
+	println!("hello"); //prints
+	println!("format {} arguments", "some"); // Prints format some arguments
+	println!("My name is {}, I am the class Rep of stream 1", "Dagogo William-Jumbo");
+
+}
