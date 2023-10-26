652154416f638c274909ae8f6048eac1b08a4e90
nawa4you <itzudochukwu@gmail.com>
2023-10-26 16:18:59 +0100

    Week 3

---
+++ week-3/practice-8/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+	// This program wont run because the mut file is not initiated
+    fn main() {
+	let fees:i32 = 25_000;
+	println!("fees changesd is {}", fees);
+
+fees = 35_000;
+println!("fees changed is {}", fees);
+
+
+}
+
+}
