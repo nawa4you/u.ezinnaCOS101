4c8dfec6a95430195bccb7cb71a40e9eea2dd874
nawa4you <itzudochukwu@gmail.com>
2023-10-26 16:01:20 +0100

    Week 3

---
+++ week-3/practice-10/target/debug/deps/practice_10.d
@@ -0,0 +1,5 @@
+C:\Users\Public\Documents\u.ezinnaCOS101\week-3\practice-10\target\debug\deps\practice_10.exe: src\main.rs
+
+C:\Users\Public\Documents\u.ezinnaCOS101\week-3\practice-10\target\debug\deps\practice_10.d: src\main.rs
+
+src\main.rs:
---
+++ week-3/practice-10/target/debug/practice-10.d
@@ -0,0 +1,1 @@
+C:\Users\Public\Documents\u.ezinnaCOS101\week-3\practice-10\target\debug\practice-10.exe: C:\Users\Public\Documents\u.ezinnaCOS101\week-3\practice-10\src\main.rs
